34332d70681c1bec507ee8955490218a1342875b
DLlllxx <dengbaicai@gmail.com>
2022-01-15 23:11:12 +0100

    interface

--- Projetjava/src/main/java/fr/dauphine/JavaAvance/GUI/GUI.java
+++ Projetjava/src/main/java/fr/dauphine/JavaAvance/GUI/GUI.java
@@ -13,6 +13,7 @@ import java.net.URL;
 import java.security.PublicKey;
 import java.security.DrbgParameters.NextBytes;
 import java.util.ArrayList;
+import java.util.IllegalFormatCodePointException;
 import java.util.Iterator;
 import java.util.Random;
 import java.awt.Dimension;
@@ -55,6 +56,9 @@ public class GUI extends JFrame implements ActionListener{
 	private JPanel controlPanel;
 	private JButton start;
 	private JFrame jFrame;
+	
+	private JPanel jPanel;
+	private static Grid grid;
 	/**
 	 * 
 	 * @param inputFile
@@ -98,6 +102,7 @@ public class GUI extends JFrame implements ActionListener{
 	public GUI(Grid grid) {
 
 		initialize(grid);
+		
 		frame.setVisible(true);
 	}
 
@@ -110,16 +115,16 @@ public class GUI extends JFrame implements ActionListener{
 		// To implement:
 		// creating frame, labels
 		// Implementing method mouse clicked of interface MouseListener.
+		
 		 frame = new JFrame("Infinite loop");
 	     frame.setSize(400,400);
 	     //frame.setLayout(new GridLayout(3, 1));
 	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    
+	     
 	     controlPanel = new JPanel();
 		 controlPanel.setLayout(new GridLayout(4,4));
 		 controlPanel.setMaximumSize(new Dimension(400,400));
-	    
-		 
+		
 	     headerLaberJLabel = new JLabel("Welcome tu game Infinite Loop", JLabel.CENTER);        
 	     headerLaberJLabel.setSize(350,100);
 	     controlPanel.add(headerLaberJLabel,BorderLayout.CENTER);
@@ -143,12 +148,17 @@ public class GUI extends JFrame implements ActionListener{
 		     aJLabel.setText("start game");
 			 aJLabel.setFont(new Font("Serif", Font.PLAIN, 30));
 			 jFrame.add(aJLabel,BorderLayout.NORTH);
-			JPanel jPanel = new JPanel();
+			jPanel = new JPanel();
 		    jPanel.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
-			for(int i=0;i<12;i++)
-			   {
-				   afficherPieces(jPanel);
-			   }
+
+			creerPiece();
+
+
+				
+				System.out.println("ok5");
+				putIntoInterface();
+				
+			   
 		     jFrame.add(jPanel);
 		     
 			 start.addActionListener(new ActionListener() {
@@ -192,6 +202,7 @@ public class GUI extends JFrame implements ActionListener{
 	 *            the piece
 	 * @return an image icon
 	 */
+//Ce m¨¦thode il y a que de photo sans connecter avec la classe Pi¨¨ce
 	private ArrayList<ImageIcon> getImageIcon(Piece p) {
 		ArrayList<ImageIcon> listImg=createImageIcon();
 		//Iterator iterator = listImg.iterator();
@@ -230,14 +241,183 @@ public class GUI extends JFrame implements ActionListener{
 	
 	}
 	
+	protected ImageIcon createImageIcon(int j) {
+		ClassLoader cldr = this.getClass().getClassLoader();
+		java.net.URL imgURL =null;
+		ArrayList<ImageIcon> imageIcons = new ArrayList<>();
+		java.net.URL imgURL1 = cldr.getResource("fr/dauphine/JavaAvance/icons/io/"+"j"+".png");
+		ImageIcon imageIcon=null;
+		for(int i = 1;i<16;i++)
+		{
+			imgURL = cldr.getResource("fr/dauphine/JavaAvance/icons/io/"+i+".png");
+			
+			if (imgURL != null) {
+				imageIcon = new ImageIcon(imgURL, "ok");
+				
+				} 
+			else {
+				System.err.println("Couldn't find file: " );
+				}
+		}
+		return imageIcon;
+		
 	
+	}
+	
+	private ImageIcon getImageIcon1(Piece p) {
+		ImageIcon imageIcon=null;
+		System.out.println("KKKK");
+		System.out.println(p.getType());
+		if(p.getType()==PieceType.BAR)
+		{
+			if(p.getOrientation()==Orientation.NORTH)
+			{
+				imageIcon = createImageIcon(5);
+			}
+			if(p.getOrientation()==Orientation.EAST) {
+				imageIcon = createImageIcon(6);
+			}
+		}
+		if(p.getType()==PieceType.ONECONN)
+		{
+			if(p.getOrientation()==Orientation.NORTH)
+			{
+				imageIcon = createImageIcon(1);
+			}
+			if(p.getOrientation()==Orientation.SOUTH) {
+				imageIcon = createImageIcon(3);
+			}
+			if(p.getOrientation()==Orientation.EAST) {
+				imageIcon = createImageIcon(2);
+			}
+			if(p.getOrientation()==Orientation.WEST) {
+				imageIcon = createImageIcon(4);
+			}
+			}
+			if(p.getType()==PieceType.TTYPE)
+			{
+				if(p.getOrientation()==Orientation.NORTH)
+				{
+					imageIcon = createImageIcon(7);
+				}
+				if(p.getOrientation()==Orientation.SOUTH) {
+					imageIcon = createImageIcon(9);
+				}
+				if(p.getOrientation()==Orientation.EAST) {
+					imageIcon = createImageIcon(8);
+				}
+				if(p.getOrientation()==Orientation.WEST) {
+					imageIcon = createImageIcon(10);
+				}
+			}
+			if(p.getType()==PieceType.LTYPE)
+			{
+				if(p.getOrientation()==Orientation.NORTH)
+				{
+					imageIcon = createImageIcon(12);
+				}
+				if(p.getOrientation()==Orientation.SOUTH) {
+					imageIcon = createImageIcon(14);
+				}
+				if(p.getOrientation()==Orientation.EAST) {
+					imageIcon = createImageIcon(13);
+				}
+				if(p.getOrientation()==Orientation.WEST) {
+					imageIcon = createImageIcon(15);
+				}
+			}
+			if(p.getType()==PieceType.FOURCONN)
+			{
+				
+					imageIcon = createImageIcon(11);
+				}
+			return imageIcon;
+	}	
+	
+	public Grid putPiece(ArrayList<Piece> p)
+	{
+		int i = grid.getHeight();
+		int j = grid.getWidth();
+		boolean bool = false;
+		Iterator<Piece> iterator = p.iterator();
+		for (int a = 0;a<i;a++)
+		{
+			for(int b = 0;b<j;b++)
+			{
+					if(p!=null) {
+					grid.setPiece(a, b, p.get(1));
+				System.out.println(p.get(0).getOrientation());
+				p.remove(1);
+
+			}
+					else {
+						grid.setPiece(a, b, null);
+					}
+			
+		}
+		}
+			System.out.println("ok1");
+		return grid;
+		
+	}
+	public void putIntoInterface()
+	{
+		ArrayList<JLabel> jl = new ArrayList<>();
+		ImageIcon imageIcon=null;
+		int i = grid.getHeight();
+		int j = grid.getWidth();
+		System.out.println(i+" "+j);
+		JLabel label = new JLabel();
+		for (int a = 0;a<i;a++)
+		{
+			for(int b = 0;b<j;b++)
+			{
+				
+					
+				imageIcon=getImageIcon1(grid.getPiece(a, b));
+			//	System.out.println(grid.getPiece(a, b).getOrientation());
+				label.setIcon(imageIcon);
+				jl.add(label);
+				
+			}
+		}
+
+		 Iterator<JLabel> iterator2 = jl.iterator();
+		 while(iterator2.hasNext())
+		 {
+			 JLabel j1=iterator2.next();
+			 jPanel.add(j1);
+		 }
+		 
+		 System.out.println("ok2");
+
+	}
 	
 	public static void main(String[] args)
 	{
-		Grid grid=new Grid(10, 10);
+		try {
+			grid= new Grid(4, 4);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		GUI gui = new GUI(grid);
 		
 		
+	}
+	public void creerPiece()
+	{
+		Piece p1 = new Piece(1, 1,PieceType.BAR,Orientation.NORTH);
+		Piece p2 = new Piece(1, 1, PieceType.BAR, Orientation.SOUTH);
+		Piece p3 = new Piece(1, 1,PieceType.BAR,Orientation.EAST);
+		Piece p4 = new Piece(1, 1, PieceType.BAR, Orientation.WEST);
+		ArrayList<Piece> pieces = new ArrayList<>();
+		pieces.add(p1);
+		pieces.add(p2);
+		pieces.add(p3);
+		pieces.add(p4);
+		grid = putPiece(pieces);
+
 	}
 
 	@Override
